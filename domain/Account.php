9c20c25c0713edd22f2321d582a4ea2cd36642aa
James Treutle <jtreutle@gmail.com>
2022-03-16 13:14:08 -0400

    Commented out the Application array and methods

--- domain/Account.php
+++ domain/Account.php
@@ -2,23 +2,13 @@
 namespace domain;
 
 /*
- * Copyright 2013 by Allen Tucker.
- * This program is part of RMHC-Homebase, which is free software.  It comes with
- * absolutely no warranty. You can redistribute and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation
- * (see <http://www.gnu.org/licenses/ for more information).
- *
- */
-
-/*
- * Created on Mar 28, 2008
- * @author Oliver Radwan <oradwan@bowdoin.edu>, Sam Roberts, Allen Tucker
- * @version 3/28/2008, revised 7/1/2015
+ * Created on Mar 8, 2022
+ * @version 3/14/2022, revised 3/16/2022
  */
  class Account {
 	private $email;   // email address as a string
 	private $password;     // password for account access: default = $email
-	private $applications;     // array of applications acosiated with this account: key=id
+	//private $applications;     // array of applications acosiated with this account: key=id
 	private $first_name;       //first name of account holder as a string
 	private $last_name;        //last name of account holder as a string
 	private $status;     // a person may be an "applicant", "active", "LOA", or "former"
@@ -33,7 +23,7 @@ namespace domain;
 			$this->password = md5($this->email);
 		else
 			$this->password = $pass;  // default password == md5($email)
-		$this->applications = array();
+		//$this->applications = array();
 	}
 
 	function get_email() {
@@ -48,13 +38,13 @@ namespace domain;
 	    $this->password = $pass;
 	}
 
-	function add_application($id, $application) {
+	/*function add_application($id, $application) {
 	    $this->applications[$id] = $application;
 	}
 
 	function get_applications() {
 	    return $this->applications;
-	}
+	}*/
 
 	function get_first_name() {
 	    return $this->first_name;
