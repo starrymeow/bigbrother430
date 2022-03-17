f12fc96f32bee6a0c6769f61a19ee5f8ee138b14
JRutled2 <81650372+JRutled2@users.noreply.github.com>
2022-03-17 12:21:50 -0400

    Revert "Revert "adding in fixes to index and replacing all instances of Person with Account in relevant pages""

--- domain/Application.php
+++ domain/Application.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 class Application {
     private $id;        //unique id for each application
     private $firstname;     //first name of the big or little
@@ -6,7 +6,7 @@ class Application {
     private $languages;     //array of strings that are the names of the known languages
     private $primarylanguage;
     private $prefered_name;      //nick name if applicable as a string
-    private $date_of_birth;
+    private $birthday;
     private $cell_phone;      //cell phone number
     private $can_text_cell;     //whether or not the cell; boolean
     private $home_phone;        //home phone number
@@ -18,25 +18,25 @@ class Application {
     private $race;
     private $apply_reason;      //reason for applying
     private $life_changes;      //recent major life changes
-    
+
     function __construct ($i, $f, $l, $langs, $prime, $name, $dob, $cell, $text, $home, $g, $a, $c, $z, $r, $apply, $life) {
-        $this->$id = $i;
-        $this->$firstname = $f; 
-        $this->$lastname = $l;
-        $this->$languages = $langs;
-        $this->$primary_language = $prime;
-        $this->$perfered_name = $name;
-        $this->$date_of_birth = $dob;
-        $this->$cell_phone = $cell;
-        $this->$can_text_cell = $text;
-        $this->$home_phone = $home;
-        $this->$gender = $g;
-        $this->$address = $a;
-        $this->$city = $c;
-        $this->$zip = $z;
-        $this->$race = $r;
-        $this->$apply_reason = $apply;
-        $this->$life_changes = $life;
+        $this->id = $i;
+        $this->firstname = $f;
+        $this->lastname = $l;
+        $this->languages = $langs;
+        $this->primary_language = $prime;
+        $this->perfered_name = $name;
+        $this->birthday = $dob;
+        $this->cell_phone = $cell;
+        $this->can_text_cell = $text;
+        $this->home_phone = $home;
+        $this->gender = $g;
+        $this->address = $a;
+        $this->city = $c;
+        $this->zip = $z;
+        $this->race = $r;
+        $this->apply_reason = $apply;
+        $this->life_changes = $life;
     }
 }
 ?>
