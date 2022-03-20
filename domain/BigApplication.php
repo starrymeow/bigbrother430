c5a6b3cd62066ac3eea5051dd0e398cbaca09a03
James Treutle <jtreutle@gmail.com>
2022-03-19 20:12:07 -0400

    Created constructors

--- domain/BigApplication.php
+++ domain/BigApplication.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 class BigApplication extends Application {
 
-    private $secondary_email; // if applicant has a second email when can reach them by
+    private $secondary_email; // if applicant has a second email we can reach them by
     private $ssn; // applicant's social security number
     private $relationship_status; // what is applicant's relationship status (i.e., married or single)
     private $orientation; // what is applicant's orientation
@@ -12,60 +12,72 @@ class BigApplication extends Application {
     private $emergency_contact; // who can we contact incase of an emergency
     private $EC_number; // the emergency contact's phone number
     private $EC_relation; // what relation does the contact have with the applicant
-    
-    //create construction function and get functions
-    
+
+    function __construct($e, $i, $f, $l, $langs, $prime, $name, $dob, $cell, $text, $home, $g, $a, $c, $z, $r, $apply, $life,
+            $se, $ssn, $relation, $o, $faith, $number, $state, $exp, $emerg, $EC_number, $EC_relation) {
+        Application::construct($e, $i, $f, $l, $langs, $prime, $name, $dob, $cell, $text, $home, $g, $a, $c, $z, $r, $apply, $life);
+        $this->secondary_email = $se;
+        $this->ssn = $ssn;
+        $this->relationship_status = $relation;
+        $this->orientation = $o;
+        $this->faith = $faith;
+        $this->DL_number = $number;
+        $this->DL_state = $state;
+        $this->emergency_contact = $emerg;
+        $this->EC_number = $EC_number;
+        $this->EC_relartion = $EC_relation;
+    }
+
     // returns the second email address
     function get_secondary_email() {
         return $this->secondary_email;
     }
-    
+
     // returns their social security number
     function get_ssn() {
         return $this->ssn;
     }
-    
+
     // returns their relationship status
     function get_relationship_status() {
         return $this->relationship_status;
     }
-    
+
     // returns their orientation
     function get_orientation() {
         return $this->orientation;
     }
-    
+
     // returns their faith/belief
     function get_faith() {
         return $this->faith;
     }
-    
+
     // returns their driver's license number
     function get_DL_number() {
         return $this->DL_number;
     }
-    
-    
+
     // returns the state that issued their driver's license
     function get_DL_state() {
         return $this->DL_state;
     }
-    
+
     // returns the expiration date of their driver's license
     function get_DL_expiration() {
         return $this->DL_expiration;
     }
-    
+
     // returns the name of their emergency contact
     function get_emergency_contact() {
         return $this->emergency_contact;
     }
-    
+
     // returns the number of their emergency contact
     function get_EC_number() {
         return $this->EC_number;
     }
-    
+
     // returns the relationship between the emergency contact and applicant
     function get_EC_relation() {
         return $this->EC_relation;
