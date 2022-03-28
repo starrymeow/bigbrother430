b13b1ccbef187a82203f0838697fee721c0b376a
James Treutle <jtreutle@gmail.com>
2022-03-28 09:38:15 -0400

    renamed PersonTest

--- tests/dbAccountTest.php
+++ tests/dbAccountTest.php
@@ -1,13 +1,13 @@
 <?php
 /*
- * Copyright 2013 by Jerrick Hoang, Ivy Xing, Sam Roberts, James Cook, 
- * Johnny Coster, Judy Yang, Jackson Moniaga, Oliver Radwan, 
- * Maxwell Palmer, Nolan McNair, Taylor Talmage, and Allen Tucker. 
- * This program is part of RMH Homebase, which is free software.  It comes with 
- * absolutely no warranty. You can redistribute and/or modify it under the terms 
+ * Copyright 2013 by Jerrick Hoang, Ivy Xing, Sam Roberts, James Cook,
+ * Johnny Coster, Judy Yang, Jackson Moniaga, Oliver Radwan,
+ * Maxwell Palmer, Nolan McNair, Taylor Talmage, and Allen Tucker.
+ * This program is part of RMH Homebase, which is free software.  It comes with
+ * absolutely no warranty. You can redistribute and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation
  * (see <http://www.gnu.org/licenses/ for more information).
- * 
+ *
  */
 
 /*
@@ -21,15 +21,15 @@ include_once(dirname(__FILE__).'/../domain/Person.php');
 class dbPersonsTest extends TestCase {
       function testdbPersons() {
       	// add two people to the database
-      
+
         $m = new Person("Gabrielle","Booth", "bangor","14 Way St", "Harpswell", "ME", 04079,
-		1112345679, "cell",7778889999,"work","ted@bowdoin.edu","volunteer","","","active", 
+		1112345679, "cell",7778889999,"work","ted@bowdoin.edu","volunteer","","","active",
 		"National Semiconductor","VP","","year","a motivation","a specialty", "",
 		"Mon:9-12:bangor,Sun:morning:bangor","","15-01-05:0930-1300:bangor:3.5","89-02-19", "08-03-14","friend","Some notes","");
 		$this->assertTrue(add_person($m));
-		
+
 		$m2 = new Person("Fred","Wilson", "portland","14 Boyer Ave", "Portland", "ME", 04503,
-		5093456789, "home",5098889999,"cell","alfred@whitman.edu","volunteer","","","active", 
+		5093456789, "home",5098889999,"cell","alfred@whitman.edu","volunteer","","","active",
 		"McDonalds","manager","","year","a motivation2","First Aid", "",
 		"Wed:9-12:portland,Sun:afternoon:portland","","15-02-27:1730-2100:portland:3.5","91-09-25","07-04-14","other","Some notes","");
 		$this->assertTrue(add_person($m2));
@@ -42,7 +42,7 @@ class dbPersonsTest extends TestCase {
 		$this->assertEquals($p->get_type(), array("volunteer"));
 		$this->assertEquals($p->get_hours(), array("15-01-05:0930-1300:bangor:3.5"));
 		$this->assertTrue($p->get_birthday() == "89-02-19");
-		
+
 		$p2 = retrieve_person("Fred5093456789");
 		$this->assertTrue($p2!==false);
 		$this->assertTrue($p2->get_status() == "active");
@@ -50,12 +50,12 @@ class dbPersonsTest extends TestCase {
 		$this->assertEquals($p2->get_type(), array("volunteer"));
 		$this->assertEquals($p2->get_hours(), array("15-02-27:1730-2100:portland:3.5"));
 		$this->assertTrue($p2->get_birthday() == "91-09-25");
-		
+
 		// remove the person
 		$this->assertTrue(remove_person("Gabrielle1112345679"));
 		$this->assertTrue(remove_person("Fred5093456789"));
-		
-		
+
+
 		echo("testdbPersons complete\n");
 
       }
