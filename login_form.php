b394ca658b25b12645e1fe847c6aad608ac5a19e
jwil956 <jwilkerson953@gmail.com>
2022-04-04 09:21:27 -0400

    password_verify

--- login_form.php
+++ login_form.php
@@ -73,8 +73,8 @@
             $account = retrieve_account($db_email);
             var_dump($account);
             if ($account) { //avoids null results
-                //if (password_verify($_POST['pass'], $account->get_password())) { //if the passwords match, login
-                if ($_POST['pass'] == $account->get_password()) {
+                if (password_verify($_POST['pass'], $account->get_password())) { //if the passwords match, login
+                //if ($_POST['pass'] == $account->get_password()) {
                 	$_SESSION['logged_in'] = 1;
                     date_default_timezone_set("America/New_York");
                     if (get_class($account) == 'admin')
