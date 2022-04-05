f4460d38e94c6f00da50d18d9f502ad291319d7c
jwil956 <jwilkerson953@gmail.com>
2022-04-05 15:35:30 -0400

    a

--- CreateAdmin.php
+++ CreateAdmin.php
@@ -41,48 +41,70 @@ session_cache_expire(30);
 
 // 				}
 				if ($_SESSION['access_level'] >= 2) {
-				    //include('accountEdit.php');
-				    include('accountvalidate.inc');
-				    echo ('<h1>Enter email for new admin</h1>');
-				    echo ('<form method="post"');
-				    echo ('<label for="email">Email</label><br>');
-				    echo ('<input type="text" name="email"<br>');
-				    echo ('<input type="submit" name="checkemail" value="Check Email" class="greenButton">');
-				    
-				    if ($_POST['email'] != "") {
-				        if (valid_email($_POST['email']) == false) {
-				            echo ('<h2>Email given is not a valid email</h2>');
-				        }
+				    include('accountValidate.inc');
+				    if ($_POST['_form_submit'] != 1)
+				        //in this case, the form has not been submitted, so show it
+				        include('accountForm.inc');
 				        else {
-				            //include('accountForm.inc');
-				            echo ('<label for="f_name">First Name</lable><br>');
-				            echo ('<input type="text" name="f_name"<br>');
-				            echo ('<label for="l_name">Last Name</lable><br>');
-				            echo ('<input type="text" name="l_name"<br>');
-				            echo ('<input type="submit" name="checkname" value="Enter" class="greenButton">');
-				            if ($_POST['f_name'] != "" && $_POST['l_name'] != "") {
-				                $account = new Account($_POST['email'], "new", $_POST['f_name'], $_POST['l_name'], "admin");
-				                //in this case, the form has been submitted, so validate it
-				                $errors = validate_account($account);  //step one is validation.
-				                // errors array lists problems on the form submitted
-				                if ($errors) {
-				                    // display the errors and the form to fix
-				                    show_errors($errors);
-				                }
-				            }
 				            //in this case, the form has been submitted, so validate it
 				            $errors = validate_account($account);  //step one is validation.
 				            // errors array lists problems on the form submitted
 				            if ($errors) {
-				               // display the errors and the form to fix
-				               show_errors($errors);
+				                // display the errors and the form to fix
+				                show_errors($errors);
+				                $account = new Account($account->get_email(), $_POST['pass'], $_POST['first_name'], $_POST['last_name'], "new");
+				                include('accountForm.inc');
 				            }
+				            // this was a successful form submission; update the database and exit
+				            else
+				                process_form($email,$account);
+				                echo "</div>";
+				                include('footer.inc');
+				                echo('</div></body></html>');
+				                die();
+				        }
+// 				    //include('accountEdit.php');
+// 				    include('accountvalidate.inc');
+// 				    echo ('<h1>Enter email for new admin</h1>');
+// 				    echo ('<form method="post"');
+// 				    echo ('<label for="email">Email</label><br>');
+// 				    echo ('<input type="text" name="email"<br>');
+// 				    echo ('<input type="submit" name="checkemail" value="Check Email" class="greenButton">');
+				    
+// 				    if ($_POST['email'] != "") {
+// 				        if (valid_email($_POST['email']) == false) {
+// 				            echo ('<h2>Email given is not a valid email</h2>');
+// 				        }
+// 				        else {
+// 				            //include('accountForm.inc');
+// 				            echo ('<label for="f_name">First Name</lable><br>');
+// 				            echo ('<input type="text" name="f_name"<br>');
+// 				            echo ('<label for="l_name">Last Name</lable><br>');
+// 				            echo ('<input type="text" name="l_name"<br>');
+// 				            echo ('<input type="submit" name="checkname" value="Enter" class="greenButton">');
+// 				            if ($_POST['f_name'] != "" && $_POST['l_name'] != "") {
+// 				                $account = new Account($_POST['email'], "new", $_POST['f_name'], $_POST['l_name'], "admin");
+// 				                //in this case, the form has been submitted, so validate it
+// 				                $errors = validate_account($account);  //step one is validation.
+// 				                // errors array lists problems on the form submitted
+// 				                if ($errors) {
+// 				                    // display the errors and the form to fix
+// 				                    show_errors($errors);
+// 				                }
+// 				            }
+// 				            //in this case, the form has been submitted, so validate it
+// 				            $errors = validate_account($account);  //step one is validation.
+// 				            // errors array lists problems on the form submitted
+// 				            if ($errors) {
+// 				               // display the errors and the form to fix
+// 				               show_errors($errors);
+// 				            }
 				            
 				          
-				      }
+// 				      }
 				   
 				        
-				    }
+// 				    }
 				}
 
 				goto end;
